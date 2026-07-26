fcf648cecc2a8ee3730f86ed40ed28a8709469e4
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-07-26 17:16:18 +0300

    Fix linked list generators

--- tests/linked_list/conftest.py
+++ tests/linked_list/conftest.py
@@ -1,26 +1,27 @@
 import random
-from collections.abc import Callable
 
 import pytest
 
 from src.linked_list import LinkedList
-from src.linked_list.node import Node
 
 
 @pytest.fixture
-def node_generator() -> Callable[[], Node]:
-    def _generator() -> Node:
-        return Node(random.randint(0, 100))
+def linked_list_generator() -> LinkedList:
+    def _generator(length: int) -> LinkedList:
+        linked_list = LinkedList()
+        for i in range(length):
+            linked_list.insert_at_end(i)
+        return linked_list
 
     return _generator
 
 
 @pytest.fixture
-def linked_list_generator(node_generator: Callable[[], Node]) -> LinkedList:
+def linked_list_generator_random() -> LinkedList:
     def _generator(length: int) -> LinkedList:
         linked_list = LinkedList()
         for _ in range(length):
-            linked_list.insert_at_end(node_generator())
+            linked_list.insert_at_end(random.randint(0, 100))
         return linked_list
 
     return _generator
--- tests/linked_list/test_reverse.py
+++ tests/linked_list/test_reverse.py
@@ -7,14 +7,14 @@ def test_reverse_empty_list():
     assert linked_list.to_list() == []
 
 
-def test_reverse_single_element_list(linked_list_generator: LinkedList):
+def test_reverse_single_element_list(linked_list_generator):
     linked_list = linked_list_generator(1)
     linked_list.reverse()
     assert linked_list.to_list() == linked_list.to_list()
 
 
-def test_reverse_multiple_elements_list(linked_list_generator: LinkedList):
-    linked_list = linked_list_generator(5)
+def test_reverse_multiple_elements_list(linked_list_generator_random):
+    linked_list = linked_list_generator_random(5)
     original_list = linked_list.to_list()
     linked_list.reverse()
     assert linked_list.to_list() == original_list[::-1]
