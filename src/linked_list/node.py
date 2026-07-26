b32d231f4ec7e16a94a0278bfada77db63ebee28
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-07-26 18:00:24 +0300

    Add repr for node

--- src/linked_list/node.py
+++ src/linked_list/node.py
@@ -1,6 +1,7 @@
 class Node:
-  def __init__(self, data=None):
-    self.data = data
-    self.next = None
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-
+    def __repr__(self) -> str:
+        return f"Node({self.data.__repr__()})"
