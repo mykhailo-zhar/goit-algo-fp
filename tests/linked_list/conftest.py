7f497e48407ea0a839c16aff68434abf55978d8b
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-07-26 18:00:08 +0300

    Refactor generators for linkedlist

--- tests/linked_list/conftest.py
+++ tests/linked_list/conftest.py
@@ -1,4 +1,4 @@
-import random
+from collections.abc import Callable
 
 import pytest
 
@@ -6,22 +6,19 @@ from src.linked_list import LinkedList
 
 
 @pytest.fixture
-def linked_list_generator() -> LinkedList:
+def linked_list_generator(linked_list_generator_custom) -> LinkedList:
     def _generator(length: int) -> LinkedList:
-        linked_list = LinkedList()
-        for i in range(length):
-            linked_list.insert_at_end(i)
-        return linked_list
+        return linked_list_generator_custom(length, lambda x: x)
 
     return _generator
 
 
 @pytest.fixture
-def linked_list_generator_random() -> LinkedList:
-    def _generator(length: int) -> LinkedList:
+def linked_list_generator_custom() -> LinkedList:
+    def _generator(length: int, callable: Callable[[int], int]) -> LinkedList:
         linked_list = LinkedList()
-        for _ in range(length):
-            linked_list.insert_at_end(random.randint(0, 100))
+        for i in range(length):
+            linked_list.insert_at_end(callable(i))
         return linked_list
 
     return _generator
