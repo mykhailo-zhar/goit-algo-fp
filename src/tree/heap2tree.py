05434febf68ffbb350a2b087d41bb645851ae724
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-08-03 15:32:08 +0300

    Update documentation

--- src/tree/heap2tree.py
+++ src/tree/heap2tree.py
@@ -5,6 +5,15 @@ from .tree import Node
 
 
 def heap_to_tree(heap: list[Any]) -> Node:
+    """
+    Transforms heap into tree
+
+    Args:
+        heap (list[Any]): Array or heap to convert
+
+    Returns:
+        Node: A root of the binary tree
+    """
     if not heap:
         return None
     local_heap = heap[:]
