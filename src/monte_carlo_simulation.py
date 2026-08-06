a1f10a49c6a8b861b14651bf7ae6e0a4b52b1c2b
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-08-06 18:50:41 +0300

    Add monte carlo simulation

---
+++ src/monte_carlo_simulation.py
@@ -0,0 +1,48 @@
+import random
+
+
+def monte_carlo_simulation(n_experiments):
+    sums = [0] * 11
+    for _ in range(n_experiments):
+        combination = random.randint(1, 6) + random.randint(1, 6)
+        sums[combination - 2] += 1
+
+    combination_sum = sum(sums)
+    return [combination / combination_sum for combination in sums]
+
+
+def print_probabilities(probabilities):
+    print(f"{'Sum':>5} | {'Probability':>12}")
+    print("-" * 21)
+    # Print the values for each sum from 2 to 12
+    for i, prob in enumerate(probabilities, start=2):
+        print(f"{i:>5} | {prob:>11.2%}")
+    print("\n")
+
+
+def print_expetiment(n_experiments, analytical):
+    result = monte_carlo_simulation(n_experiments)
+    print(f"Monte carlo simulation for {n_experiments} experiments")
+    print(f"{'Sum':>5} | {'Probability':>12} | {'Error':>7}")
+    print("-" * 36)
+    # Print the values for each sum from 2 to 12
+    for i, (prob, analytical_prob) in enumerate(zip(result, analytical), start=2):
+        print(f"{i:>5} | {prob:>11.2%}  | {abs(prob - analytical_prob)*100:>9e}")
+    print("\n")
+
+
+def main():
+    analytical_result = [i / 36 for i in range(1, 7)] + [
+        i / 36 for i in range(5, 1, -1)
+    ]
+    print("Analytical result for cube toss sum probabilities: ")
+    print_probabilities(analytical_result)
+    print_expetiment(1000, analytical_result)
+    print_expetiment(10000, analytical_result)
+    print_expetiment(100_000, analytical_result)
+    print_expetiment(1_000_000, analytical_result)
+    print_expetiment(10_000_000, analytical_result)
+
+
+if __name__ == "__main__":
+    main()
